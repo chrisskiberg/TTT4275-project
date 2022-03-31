7cbbb0476a3228502cbe62ee781a832f3819c6cd
Niels bakmann <49429726+niebak@users.noreply.github.com>
2022-03-31 17:56:10 +0200

    Add files via upload
    Code for beginnings of both task 1(iris) and task 2(MNIST)

---
+++ iristask1.m
@@ -0,0 +1,114 @@
+%Data from BB, Iris(flower) classification script
+%% preparing data, and seperating into training and testing subsets
+load("class_1");
+load("class_2");
+load("class_3");
+%The number of points used for training or testing
+ntrain=30;
+ntest=20;
+
+%definitions
+nfeat=4;
+nclass=3;
+
+limit=0.6;
+alpha=0.01;
+
+%seperating training and testing data
+% To do task1a switch the indexes in these lines to fit the assigned 
+% data-points
+c1train=class_1(1:ntrain,:);
+c1test=class_1(ntrain+1:ntest+ntrain,:);
+
+c2train=class_2(1:ntrain,:);
+c2test=class_2(ntrain+1:ntest+ntrain,:);
+
+c3train=class_3(1:ntrain,:);
+c3test=class_3(ntrain+1:ntest+ntrain,:);
+% 
+% c1train=class_1(ntest+1:ntest+ntrain,:);
+% c1test=class_1(1:ntest,:);
+% 
+% c2train=class_2(ntest+1:ntest+ntrain,:);
+% c2test=class_2(1:ntest,:);
+% 
+% c3train=class_3(ntest+1:ntest+ntrain,:);
+% c3test=class_3(1:ntest,:);
+
+%Collecting data and defining targets
+training=[c1train;c2train;c3train];
+test=[c1test;c2test;c3test];
+
+T1=[1;0;0];
+T2=[0;1;0];
+T3=[0;0;1];
+
+%% Setting up equation (22) from compendium(Classification section), and the sigmoid to be used later
+grad_W_MSE_k = @(gk, tk, xk) ( (gk - tk) .* gk .* (1 - gk) ) * xk';
+sigmoid=@(x) (1./(1+exp(-x)));
+%% Training the model
+lim=limit;
+t=[kron(ones(1,ntrain),T1),kron(ones(1,ntrain),T2),kron(ones(1,ntrain),T3)];
+w=eye(nclass,nfeat+1);
+condition=1;
+
+fprintf('Training commences \n')
+tic;
+while condition
+    W_MSE=0;
+    for k=1:nclass*ntrain
+        xk=[training(k,:)'; 1];
+        zk=w*xk;
+        gk=sigmoid(zk);
+        tk=t(:,k);
+        W_MSE=W_MSE+grad_W_MSE_k(gk,tk,xk);
+    end
+    condition=norm(W_MSE)>=lim;
+
+    a=alpha;
+    w=w-a*W_MSE;
+end
+timespent=toc;
+fprintf('Training complete!\n Spent %3.6f s training \n',timespent)
+
+%% Testing the model trained above, and plotting results in a confusion matrix.
+% Predictions available in test_pred, amount of correct classifications in
+% ccn, where n is the class.
+test_known=[kron(ones(1,ntest),T1),kron(ones(1,ntest),T2),kron(ones(1,ntest),T3)];
+test_pred=zeros(size(test_known));
+
+for i=1:length(test)
+    x=[test(i,:)'; 1];
+    g=sigmoid(w*x);
+    [f,j] = max(g);
+    test_pred(j,i)=1;
+end
+
+train_pred=zeros(size(t));
+for i=1:length(training)
+    x=[training(i,:)'; 1];
+    g=sigmoid(w*x);
+    [f,j] = max(g);
+    train_pred(j,i)=1;
+end    
+
+figure(1);
+% Please note that the plotconfusion requires the deeplearning toolbox for
+% matlab
+plotconfusion(test_known,test_pred,'Test set, alpha=0.01');
+titl = get(get(gca,'title'),'string');
+title({titl, ['30 last points training,20 first points testing, alpha=0.01']});
+xticklabels({'Setosa', 'Versicolour', 'Virginica'});
+yticklabels({'Setosa', 'Versicolour', 'Virginica'});
+
+
+figure(2);
+plotconfusion(t,train_pred,'Training set');
+titl = get(get(gca,'title'),'string');
+title({titl, '30 last points training,20 first points testing, alpha=0.01'});
+xticklabels({'Setosa', 'Versicolour', 'Virginica'});
+yticklabels({'Setosa', 'Versicolour', 'Virginica'});
+
+cc1=sum(test_pred(1,1:20));
+cc2=sum(test_pred(2,21:40));
+cc3=sum(test_pred(3,41:60));
---
+++ mnisttask1.m
@@ -0,0 +1,60 @@
+%% MNIST digit classification, data from Blackboard(using data_all.mat)
+% Start by clearing everything else, and loading data
+clear all
+load("data_all.mat");
+%full test/train data lengths
+ntest=size(testv,1);
+ntrain=size(trainv,1);
+message='\nThis script will classify digits usign data from assigned.\nClassification is done with NN-alogrithm\n';
+fprintf(message)
+%creating batches
+testbatch=ntest/1;
+trainbatch=ntrain/1;
+fprintf('Testbatches of %i samples.\nTrainingbatches of %i samples. \n',testbatch,trainbatch)
+
+%% 1NN classification (only on one testbatch)!
+% This section also plots the confmat, as just classification is
+% meaningless unless we can display the results properly
+
+guess=zeros(10,testbatch);
+tic;
+
+% Purely cosmetic
+name='Image to be classified.';
+imfig=figure('Name',name);
+ax1=axes(imfig);
+X=zeros(28,28);
+f = waitbar(0, 'Starting');
+
+%classification:
+for i=1:testbatch
+    point=testv(i,:);
+    distances=dist(trainv,point');
+    [dmin,minind]=min(distances);
+    prediction=trainlab(minind);
+    guess(prediction+1,i)=1;
+    X(:)=testv(i,:);
+    x=fliplr(imrotate(X,270));
+    image(ax1,x)
+    waitbar(i/testbatch, f, sprintf(['\nClosing this will stop classification.\n Point %i was classified as %i'],i,prediction+1));
+end
+time=toc;
+fprintf('Spent %2.2f minutes classifying using Nearest neighbour algorithm\n',time/60)
+close(f)
+close(imfig)
+
+% Preparing the true classes/labels
+true=zeros(10,testbatch);
+for i = 1:testbatch
+    t=testlab(i);
+    known(t+1,i) = 1;
+end
+% %% Plotting
+%plottting confmat
+% Please note that the plotconfusion requires the deeplearning toolbox for
+% matlab
+plotconfusion(known,guess);
+titl = get(get(gca,'title'),'string');
+title({titl, '1-NN with no clustering'});
+xticklabels({'0','1','2','3','4','5','6','7','8','9'});
+yticklabels({'0','1','2','3','4','5','6','7','8','9'});
