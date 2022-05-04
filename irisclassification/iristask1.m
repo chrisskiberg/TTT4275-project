ca1e6321b611d5dda6383fa79c4fd44759f5506e
Niels bakmann <49429726+niebak@users.noreply.github.com>
2022-05-04 09:37:58 +0200

    Comments
    Made the script a bit easier to follow along

--- irisclassification/iristask1.m
+++ irisclassification/iristask1.m
@@ -56,7 +56,7 @@ fprintf('Training commences \n')
 tic;
 while c
     W_MSE=0;
-    for k=1:nclass*ntrain%Here we realise the theory given in the report.
+    for k=1:nclass*ntrain%Here we realise the theory given in the report, approx section 2
         xk=[training(k,:)'; 1];
         zk=w*xk;
         gk=sigmoid(zk);
@@ -64,8 +64,9 @@ while c
         W_MSE=W_MSE+grad_W_MSE_k(gk,tk,xk);
     end
     c=norm(W_MSE)>=limit;
-
-    w=w-alpha*W_MSE;%Update the weights after the entire test set is run through. This could be done at more regular steps, but this was not inlcuded in the task.
+    %Update the weights after the entire test set is run through. This could be done at more regular steps, but this was not inlcuded in the task.
+    %This is known as batches in general ML.
+    w=w-alpha*W_MSE;
 end
 timespent=toc;
 fprintf('Training complete!\n Spent %3.6f s training \n',timespent)
@@ -91,7 +92,7 @@ for i=1:length(training)
     train_pred(j,i)=1;
 end    
 
-figure(1);
+figure(1);%Prediction for the test set
 % Please note that the plotconfusion requires the deeplearning toolbox for
 % matlab
 plotconfusion(test_known,test_pred,'Test set, alpha=0.01');%As mentioned, this function requires the data to be in a one-hot structure, which it is
@@ -101,13 +102,13 @@ xticklabels({'Setosa', 'Versicolour', 'Virginica'});
 yticklabels({'Setosa', 'Versicolour', 'Virginica'});
 
 
-figure(2);
+figure(2);%Prediction for the training set
 plotconfusion(t,train_pred,'Training set');
 titl = get(get(gca,'title'),'string');
 title({titl, '30 last points training,20 first points testing, alpha=0.01'});
 xticklabels({'Setosa', 'Versicolour', 'Virginica'});
 yticklabels({'Setosa', 'Versicolour', 'Virginica'});
 
-cc1=sum(test_pred(1,1:20));
+cc1=sum(test_pred(1,1:20));%Here the sum of correctly classified points will be.
 cc2=sum(test_pred(2,21:40));
 cc3=sum(test_pred(3,41:60));
