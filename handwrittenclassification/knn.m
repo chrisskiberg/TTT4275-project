fa13e7c8a05e16521d5fcdd64ded718fbad86a10
Niels bakmann <49429726+niebak@users.noreply.github.com>
2022-05-04 11:00:19 +0200

    Comments and spelling mistakes

--- handwrittenclassification/knn.m
+++ handwrittenclassification/knn.m
@@ -6,9 +6,9 @@ load full_set.mat
 load clustered_data.mat
 
 %% Setting up definitions
-k=7;
+k=7;%Define how many neighbours to consider
 ntest=size(testv,1);
-ntrain=size(clustereddata,1);
+ntrain=size(clustereddata,1);%We are performing the classification with clustered data
 
 message=['\nThis script will classify digits usign data from assigned.' ...
     '\nClassification is done with %i NN-alogrithm, on clustered data.\n'];
@@ -18,9 +18,8 @@ testbatch=ntest/1;
 trainbatch=ntrain/1;
 fprintf(['Testbatches of %i samples.\nTrainingbatches of %i samples.\n\n'],testbatch,trainbatch)
 
-%% 1NN classification (only on one testbatch)!
-% This section also plots the confmat, as just classification is
-% meaningless unless we can display the results properly
+%% 1NN classification
+
 
 guess=zeros(10,testbatch);
 tic;
@@ -31,32 +30,34 @@ for i=1:testbatch
     point=testv(i,:);
     distances=dist(clustereddata,point');
 
-    [temp,minind]=sort(distances);%"Sort" places lowest values lowest
+    [temp,minind]=sort(distances);%"Sort" places lowest values at low indexes
     ksmallest=minind(1:k);%Take the indexes from the lowest distances
+                         % Since we are to use 7 neighbours, we take the seven lowest indexes and use them for our prediction
+                         % this is where the 7-NN part is visible. other than this(and l. 38->40) its not large changes
 
-    setedge=[-0.5 0.5 1.5 2.5 3.5 4.5 5.5 6.5 7.5 8.5 9.5 10.5];
-    [N,edges]=histcounts(truecluster(ksmallest),setedge);
-    [val,ind]=max(N);
+    setedge=[-0.5 0.5 1.5 2.5 3.5 4.5 5.5 6.5 7.5 8.5 9.5 10.5];   %There is most likely a prettier version of doing this, but this works.
+    [N,edges]=histcounts(truecluster(ksmallest),setedge);% This function counts the values in the ksmallest vector
+    [val,ind]=max(N);%here we take the class that is most frequent as defined above. This is our prediction
 
-    %prediction=truecluster(minind);
     prediction=ind-1;
     guess(prediction+1,i)=1;
 end
 time=toc;
-fprintf('Spent %2.2f minutes classifying using Nearest neighbour algorithm\n',time/60)
+fprintf('Spent %2.2f minutes classifying using Nearest neighbour algorithm\n',time/60)%being scarred from the ~30 mins spent by 1nn unclustered we still use minutes
+                                                                                      % but the script use seconds instead.
 
 
 % Preparing the true classes/labels
 known=zeros(10,testbatch);
 for i = 1:testbatch
     t=testlab(i);
-    known(t+1,i) = 1;
+    known(t+1,i) = 1;%One hot structure
 end
 % %% Plotting
 %plottting confmat
 % Please note that the plotconfusion requires the deeplearning toolbox for
 % matlab
-plotconfusion(known,guess);
+plotconfusion(known,guess);%requires one hot structure
 titl = get(get(gca,'title'),'string');
 title({titl, '7-NN with M=64 clustering'});
 xticklabels({'0','1','2','3','4','5','6','7','8','9'});
