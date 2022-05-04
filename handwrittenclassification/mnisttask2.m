d3b1e45df38a6495cc866d5e104f664d4fa4e0e1
Niels bakmann <49429726+niebak@users.noreply.github.com>
2022-05-04 10:48:50 +0200

    Comments and spelling errors

--- handwrittenclassification/mnisttask2.m
+++ handwrittenclassification/mnisttask2.m
@@ -7,9 +7,9 @@ load clustered_data.mat
 
 %% Setting up definitions
 ntest=size(testv,1);
-ntrain=size(clustereddata,1);
-
-message=['\nThis script will classify digits usign data from assigned.' ...
+ntrain=size(clustereddata,1);%Here we switch the trainingdata with the clustered data, and thus change the classifier
+                             %Not much else is different due to this intermediate step being used.
+message=['\nThis script will classify digits using data from assigned.' ...
     '\nClassification is done with NN-alogrithm, on clustered data\n'];
 fprintf(message)
 %creating batches
@@ -19,10 +19,8 @@ fprintf(['Testbatches of %i samples.\nTrainingbatches of %i samples.\n' ...
     'Working with clustered data. \n'],testbatch,trainbatch)
 
 %% 1NN classification (only on one testbatch)!
-% This section also plots the confmat, as just classification is
-% meaningless unless we can display the results properly
 
-guess=zeros(10,testbatch);
+guess=zeros(10,testbatch);%One hot structure
 tic;
 fprintf('Starting classification\n');
 % Purely cosmetic
@@ -35,7 +33,7 @@ f = waitbar(0, 'Starting');
 %classification:
 for i=1:testbatch
     point=testv(i,:);
-    distances=dist(clustereddata,point');
+    distances=dist(clustereddata,point');%Classification is done here and line under. We use clustered data, as seen by the variable name.
     [dmin,minind]=min(distances);
     prediction=truecluster(minind);
     guess(prediction+1,i)=1;
@@ -52,8 +50,9 @@ close(imfig)
 % Preparing the true classes/labels
 known=zeros(10,testbatch);
 for i = 1:testbatch
-    t=testlab(i);
-    known(t+1,i) = 1;
+    t=testlab(i);%Although we switched the training to clustered data, nothing happens to the testset, as this cant be clustered and still work as intended
+                % (We would then not perform anything other than clustering on the test set and this is not desired.)
+    known(t+1,i) = 1;%One hot
 end
 % %% Plotting
 %plottting confmat
